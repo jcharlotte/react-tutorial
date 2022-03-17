2b3d1cfd3aadb6e67a10446ca5e6dcbb8902fefb
squizu <squizu@gmail.com>
2022-03-17 21:40:21 +0100

    everything's ready for the next step

--- free code camp/fun facts/src/components/Main.js
+++ free code camp/fun facts/src/components/Main.js
@@ -1,15 +1,7 @@
 import React from "react"
 
 export default function Main() {
-    return (
-      <div className="container main">
-        <h1>Reasons I'm happy to learn React</h1>
-        <ol>
-          <li>Makes my life easier</li>
-          <li>Have better looking websites</li>
-          <li>It looks like lot of fun</li>
-        </ol>
-      </div>
-  
+    return(
+        <h1>Main goes there</h1>
     )
-  }
+}
--- free code camp/fun facts/src/index.js
+++ free code camp/fun facts/src/index.js
@@ -1,18 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer from "./components/Footer"
-import "./style.css"
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  )
-}
+import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"))
