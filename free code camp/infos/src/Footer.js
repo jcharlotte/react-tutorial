86d0671be5b9946168784deb8aac841f29880cb3
squizu <squizu@gmail.com>
2022-03-17 19:59:48 +0100

    start a new app in order to have everything work properly

---
+++ free code camp/infos/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <small>© 2022 Charlotte J. development. All rights reserved.</small>
+        </div>
+      </footer>
+    )
+  }
---
+++ free code camp/infos/src/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Main from "./Main"
+import Footer from "./Footer"
+import "./index.css"
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
