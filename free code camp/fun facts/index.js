e4d46f13dbdba7404eb52c9be789de070a734d37
squizu <squizu@gmail.com>
2022-03-17 15:02:44 +0100

    custom components part2

--- free code camp/fun facts/index.js
+++ free code camp/fun facts/index.js
@@ -1,27 +1,39 @@
 /**
 Challenge: 
 
-Part 1: Create a page of your own using a custom Page component
+Part 2: 
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
-It should return an ordered list with the reasons why you're
-excited to be learning React :)
-
-Render your list to the page
-
-//  */
 // import React from "react"
 // import ReactDOM from "react-dom"
 
 function Reasons() {
-    return (
-        <div>
-            <ol>
-                <li>Makes my life easier</li>
-                <li>Have better looking websites</li>
-                <li>It looks like lot of fun</li>
-            </ol>
-        </div>
-    )
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="react-logo.png" width="40px"></img>
+        </nav>
+      </header>
+      <h1>Reasons I'm happy to learn React</h1>
+      <ol>
+        <li>Makes my life easier</li>
+        <li>Have better looking websites</li>
+        <li>It looks like lot of fun</li>
+      </ol>
+      <footer>
+        <p>© 2022 Charlotte J. development. All rights reserved.</p>
+      </footer>
+    </div>
+  )
 }
 
 
