88345522ad74a7a1ecb356952a5335acd6f82bd2
squizu <squizu@gmail.com>
2022-03-17 14:50:20 +0100

    custom components part 1

--- free code camp/fun facts/index.js
+++ free code camp/fun facts/index.js
@@ -1,30 +1,29 @@
-/*
-Challenge: Starting from scratch, build and render the 
-HTML for our section project. Check the Google slide for 
-what you're trying to build.
+/**
+Challenge: 
 
-We'll be adding styling to it later.
+Part 1: Create a page of your own using a custom Page component
 
-Hints:
-* The React logo is a file in the project tree, so you can
-  access it by using `src="./react-logo.png" in your image
-  element
-* You can also set the `width` attribute of the image element
-  just like in HTML. In the slide, I have it set to 40px
- */
+It should return an ordered list with the reasons why you're
+excited to be learning React :)
 
-const page = (
-    <div>
-    <img src='./react-logo.png' width="40px"></img>
-    <h1>Fun Facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-    </div>
-)
+Render your list to the page
 
-ReactDOM.render( page, document.getElementById("root"))
+//  */
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+function Reasons() {
+    return (
+        <div>
+            <ol>
+                <li>Makes my life easier</li>
+                <li>Have better looking websites</li>
+                <li>It looks like lot of fun</li>
+            </ol>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(<Reasons />, document.getElementById("root"))
