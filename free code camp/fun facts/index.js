d03929da51777544ecfe84e0814d44e5b4f2f8a2
squizu <squizu@gmail.com>
2022-03-17 17:02:43 +0100

    parent/child component

--- free code camp/fun facts/index.js
+++ free code camp/fun facts/index.js
@@ -1,41 +1,54 @@
-/**
-Challenge: 
-
-Part 2: 
-- Add a `header` element with a nested `nav` element. Inside the `nav`,
-  include a `img` element with the image of the React logo inside
-  (src="./react-logo.png") and make sure to set the width to something
-  more manageable so it doesn't take up the whole screen
-- Add an `h1` with some text describing the page. (E.g. "Reasons
-  I'm excited to learn React"). Place it above the ordered list.
-- Add a `footer` after the list that says: 
-    "© 20xx <last name here> development. All rights reserved."
- */
-
 // import React from "react"
 // import ReactDOM from "react-dom"
 
-function Reasons() {
+/**
+Mini Challenge:
+Move the `header` element from Page into 
+its own component called "Header"
+*/
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="react-logo.png" width="40px" />
+      </nav>
+    </header>
+  )
+}
+
+function Main() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="react-logo.png" width="40px"></img>
-        </nav>
-      </header>
       <h1>Reasons I'm happy to learn React</h1>
       <ol>
         <li>Makes my life easier</li>
         <li>Have better looking websites</li>
         <li>It looks like lot of fun</li>
       </ol>
-      <footer>
-        <p>© 2022 Charlotte J. development. All rights reserved.</p>
-      </footer>
     </div>
+
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Charlotte J. development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
   )
 }
 
 
 
-ReactDOM.render(<Reasons />, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
