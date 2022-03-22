8962cac1821f3de2af647267b5632bf3434825c2
squizu <squizu@gmail.com>
2022-03-22 15:16:01 +0100

    starting to use props

--- free code camp/airbnb/src/App.js
+++ free code camp/airbnb/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 import "./style.css"
 
 /*
@@ -12,7 +13,8 @@ export default function App() {
     return (
         <div>
             <Navbar />
-            <Hero />
+            {/* <Hero /> */}
+            <Card />
         </div>
     )
 }
