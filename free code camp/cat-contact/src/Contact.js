c73c4205340cb2aaaa600dd62e33821dfaa146d8
squizu <squizu@gmail.com>
2022-03-23 12:25:59 +0100

    learning about destructuring

--- free code camp/cat-contact/src/Contact.js
+++ free code camp/cat-contact/src/Contact.js
@@ -2,19 +2,18 @@ import React from "react"
 import phoneIcon from "./images/phone-icon.png"
 import mailIcon from "./images/mail-icon.png"
 
-export default function Contact(props) {
-    console.log(props)
+export default function Contact({img, name, phone, email}) {
     return (
         <div className="contact-card">
-            <img src={props.img} />
-            <h3>{props.name}</h3>
+            <img src={img} />
+            <h3>{name}</h3>
             <div className="info-group">
                 <img src={phoneIcon} />
-                <p>{props.phone}</p>
+                <p>{phone}</p>
             </div>
             <div className="info-group">
                 <img src={mailIcon} />
-                <p>{props.email}</p>
+                <p>{email}</p>
             </div>
         </div>
     )
